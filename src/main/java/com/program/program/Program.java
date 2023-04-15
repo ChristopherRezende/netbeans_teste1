b6758e8de0d12393f25f6e11f8bc7741c5a940fa
ChristopherRezende <117112373+ChristopherRezende@users.noreply.github.com>
2023-04-15 18:29:39 -0300

    comit inicial

---
+++ src/main/java/com/program/program/Program.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.program.program;
+
+/**
+ *
+ * @author rezen
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        Home home = new Home();
+        home.pack();
+        home.setVisible(true);
+    }
+}
